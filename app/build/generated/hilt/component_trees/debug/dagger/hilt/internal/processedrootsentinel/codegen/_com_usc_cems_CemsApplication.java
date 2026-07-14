d8a874da8bdcd78c14c31fbe914d01c44a1ed964
ScarXiFy <johnenrico0828@gmail.com>
2026-07-14 13:34:36 +0800

    [enrico][update] .gitignore and remove generated build artifacts

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_usc_cems_CemsApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.usc.cems.CemsApplication"
-)
-public final class _com_usc_cems_CemsApplication {
-}
