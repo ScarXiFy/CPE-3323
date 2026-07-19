06b8e45e783244fe58f73d2935c051a3ccfd3ebf
mz-dy <mileahzoeiody@gmail.com>
2026-07-19 12:19:33 +0800

    [Mileah][Fix] Fixed gradle sync error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation("androidx.compose.material:material-icons-extended")
+    implementation(libs.androidx.compose.material.icons.extended)
 
     // Tooling
     debugImplementation(libs.androidx.compose.ui.tooling)
